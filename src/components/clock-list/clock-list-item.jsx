fa7bbc485835e4b7f91ab300586e2b77616b1705
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-07-17 09:09:08 +0600

    timer implemented for all clocks successfully

--- src/components/clock-list/clock-list-item.jsx
+++ src/components/clock-list/clock-list-item.jsx
@@ -1,17 +1,19 @@
 import { formatDistance } from "date-fns";
 import useClock from "../../hooks/useClock";
+import useTimer from "../../hooks/useTimer";
 import ClockDisplay from "../shared/clock-display";
 import ClockActions from "../shared/clock-actions";
 
 const ClockListItem = ({ clock, updateClock, deleteClock, localClock }) => {
   const { date } = useClock(clock.timezone, clock.offset);
+  const timer = useTimer(date);
 
-  if (!date) return null;
+  if (!date || !timer) return null;
 
   return (
     <div>
       <ClockDisplay
-        date={date}
+        date={timer}
         offset={clock.offset}
         timezone={clock.timezone}
         title={clock.title}
