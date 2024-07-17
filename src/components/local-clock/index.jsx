4bebf63de5f790a70d727ae80e3f52f1766790f8
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-07-17 09:05:16 +0600

    timer added for local clock successfully

--- src/components/local-clock/index.jsx
+++ src/components/local-clock/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import useClock from "../../hooks/useClock";
+import useTimer from "../../hooks/useTimer";
 import ClockDisplay from "../shared/clock-display";
 import ClockActions from "../shared/clock-actions";
 
@@ -15,18 +16,24 @@ const LocalClock = ({ clock, updateClock, createClock }) => {
   // Custom hook to get the current date, timezone, and offset based on the provided timezone and offset
   const { date, timezone, offset } = useClock(clock.timezone, clock.offset);
 
+  const timer = useTimer(date);
+
   /**
    * Effect hook to update the clock object with the current date whenever the date changes.
    */
   useEffect(() => {
-    updateClock({ date, timezone, offset });
+    updateClock({
+      date,
+      timezone,
+      offset,
+    });
   }, [date]);
 
   return (
     <div>
-      {date && (
+      {timer && (
         <ClockDisplay
-          date={date}
+          date={timer}
           timezone={timezone}
           offset={offset}
           title={clock.title}
