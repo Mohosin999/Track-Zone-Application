fedb583980a8dde16942689fdd8f543915328c45
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-07-27 07:14:26 +0600

    git merge successfull of event features.

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,3 @@
-// import { useState } from "react";
-// import { generate } from "shortid";
-// import ClockList from "./components/clock-list";
-// import LocalClock from "./components/local-clock";
-// import EventList from "./components/event-list";
-// // import EventForm from "./components/shared/event-form";
-
-// const LOCAL_CLOCK_INIT = {
-//   title: "My Clock",
-//   timezone: "",
-//   offset: 0,
-//   date: null,
-// };
-
-// const App = () => {
-//   const [localClock, setLocalClock] = useState({ ...LOCAL_CLOCK_INIT });
-//   const [clocks, setClocks] = useState([]);
-
-//   // Function for update local clock
-//   const updateLocalClock = (data) => {
-//     setLocalClock({
-//       ...localClock,
-//       ...data,
-//     });
-//   };
-
-//   // Function for create clock
-//   const createClock = (clock) => {
-//     clock.id = generate(); // Add an id with new created clock
-//     setClocks([...clocks, clock]);
-//   };
-
-//   // Function for update clock
-//   const updateClock = (updatedClock) => {
-//     const updatedClocks = clocks.map((clock) => {
-//       if (clock.id === updatedClock.id) {
-//         return updatedClock;
-//       }
-//       return clock;
-//     });
-
-//     setClocks(updatedClocks);
-//   };
-
-//   // Function for delete clock
-//   const deleteClock = (id) => {
-//     const deletedClocks = clocks.filter((clock) => clock.id !== id);
-//     setClocks(deletedClocks);
-//   };
-
-//   return (
-//     <div>
-//       <LocalClock
-//         clock={localClock}
-//         updateClock={updateLocalClock}
-//         createClock={createClock}
-//       />
-//       <ClockList
-//         clocks={clocks}
-//         localClock={localClock.date}
-//         updateClock={updateClock}
-//         deleteClock={deleteClock}
-//       />
-//       {/* <EventForm /> */}
-//       {/* <EventList /> */}
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import { useEffect, useState } from "react";
 import { generate } from "shortid";
 import ClockList from "./components/clock-list";
@@ -139,8 +68,6 @@ const App = () => {
         updateClock={updateClock}
         deleteClock={deleteClock}
       />
-      {/* <EventForm /> */}
-      {/* <EventList /> */}
     </div>
   );
 };
--- src/components/clock-list/clock-list-item.jsx
+++ src/components/clock-list/clock-list-item.jsx
@@ -1,119 +1,11 @@
-// import { useState } from "react";
-// import { formatDistance } from "date-fns";
-// import useClock from "../../hooks/useClock";
-// import useTimer from "../../hooks/useTimer";
-// import ClockDisplay from "../shared/clock-display";
-// import ClockActions from "../shared/clock-actions";
-// import EventForm from "../shared/event-form";
-// import EventList from "../event-list";
-// import useEvents from "../../hooks/useEvents";
-
-// const ClockListItem = ({ clock, updateClock, deleteClock, localClock }) => {
-//   /** =====================================
-//    *        Event Related Start
-//    ====================================== */
-//   const [isCreateEvent, setIsCreateEvent] = useState(false);
-//   const [toggleEvent, setToggleEvent] = useState(true);
-
-//   const { addEvent, deleteEvent, getEventsByClockId, updateEvent } =
-//     useEvents();
-//   const events = getEventsByClockId(clock.id);
-
-//   /** =====================================
-//    *        Event Related End
-//    ====================================== */
-
-//   const { date } = useClock(clock.timezone, clock.offset);
-//   const timer = useTimer(date);
-
-//   if (!date || !timer) return null;
-
-//   return (
-//     <div>
-//       <ClockDisplay
-//         date={timer}
-//         offset={clock.offset}
-//         timezone={clock.timezone}
-//         title={clock.title}
-//       />
-//       <ClockActions
-//         clock={clock}
-//         updateClock={updateClock}
-//         deleteClock={deleteClock}
-//       />
-
-//       {/* Time distance */}
-//       <h3>Time difference: {formatDistance(localClock, date)}</h3>
-
-//       {/* ==========================================================================
-//                               Event Management
-//       ====================================================================== */}
-//       <div>
-//         <div style={{ marginLeft: "50%" }}>
-//           <button
-//             style={{ marginRight: "8px" }}
-//             onClick={() => setIsCreateEvent(!isCreateEvent)}
-//           >
-//             Create Event
-//           </button>
-//           <button
-//             onClick={() => setToggleEvent(!toggleEvent)}
-//             disabled={events.length === 0}
-//           >
-//             Toggle Event
-//           </button>
-//         </div>
-
-//         {isCreateEvent && (
-//           // <h1>Event Management</h1>
-//           <EventForm
-//             clockId={clock.id}
-//             addEvent={addEvent}
-//             isCreateEvent={isCreateEvent}
-//             setIsCreateEvent={setIsCreateEvent}
-//           />
-//         )}
-
-//         {toggleEvent && (
-//           <EventList
-//             clockId={clock.id}
-//             events={events}
-//             deleteEvent={deleteEvent}
-//             updateEvent={updateEvent}
-//           />
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ClockListItem;
-
-import { useState } from "react";
 import { formatDistance } from "date-fns";
 import useClock from "../../hooks/useClock";
 import useTimer from "../../hooks/useTimer";
 import ClockDisplay from "../shared/clock-display";
 import ClockActions from "../shared/clock-actions";
-import EventForm from "../shared/event-form";
 import EventList from "../event-list";
-import useEvents from "../../hooks/useEvents";
 
 const ClockListItem = ({ clock, updateClock, deleteClock, localClock }) => {
-  /** =====================================
-   *        Event Related Start
-   ====================================== */
-  // const [isCreateEvent, setIsCreateEvent] = useState(false);
-  // const [toggleEvent, setToggleEvent] = useState(true);
-
-  // const { addEvent, deleteEvent, getEventsByClockId, updateEvent } =
-  //   useEvents();
-  // const events = getEventsByClockId(clock.id);
-
-  /** =====================================
-   *        Event Related End
-   ====================================== */
-
   const { date } = useClock(clock.timezone, clock.offset);
   const timer = useTimer(date);
 
@@ -136,45 +28,7 @@ const ClockListItem = ({ clock, updateClock, deleteClock, localClock }) => {
       {/* Time distance */}
       <h3>Time difference: {formatDistance(localClock, date)}</h3>
 
-      {/* ==========================================================================
-                              Event Management
-      ====================================================================== */}
-      <div>
-        {/* <div style={{ marginLeft: "50%" }}>
-          <button
-            style={{ marginRight: "8px" }}
-            onClick={() => setIsCreateEvent(!isCreateEvent)}
-          >
-            Create Event
-          </button>
-          <button
-            onClick={() => setToggleEvent(!toggleEvent)}
-            disabled={events.length === 0}
-          >
-            Toggle Event
-          </button>
-        </div> */}
-
-        {/* {isCreateEvent && (
-          // <h1>Event Management</h1>
-          <EventForm
-            clockId={clock.id}
-            addEvent={addEvent}
-            isCreateEvent={isCreateEvent}
-            setIsCreateEvent={setIsCreateEvent}
-          />
-        )}
-
-        {toggleEvent && (
-          <EventList
-            clockId={clock.id}
-            events={events}
-            deleteEvent={deleteEvent}
-            updateEvent={updateEvent}
-          />
-        )} */}
-        <EventList clockId={clock.id} />
-      </div>
+      <EventList clockId={clock.id} />
     </div>
   );
 };
